8088291d424a00e15c68b20036a28b14972a93f8
Hüseyin ÇALIŞKAN <huseyinncaliskann@outlook.com>
2023-01-20 11:24:31 +0300

    Generic repository and results added.

---
+++ Core/Utilities/Business/TryCatchHelper.cs
@@ -0,0 +1,26 @@
+﻿using Core.Utilities.Results.OperationResult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Busines
+{
+    public class TryCatchHelper
+    {
+        public static IResult RunTheMethod(Action method)
+        {
+            try
+            {
+                method();                
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResult(ex.Message);
+            }
+
+            return new SuccessResult();
+        }
+    }
+}
