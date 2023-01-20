80f4a009a2c558a657665600ffd9533d702eb4be
Hüseyin ÇALIŞKAN <huseyinncaliskann@outlook.com>
2023-01-20 11:26:09 +0300

    Context and dal added.

---
+++ DataAccess/Contexts/Context.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Contexts
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=HUSEYIN\SQLEXPRESS;Database=FuelWebServiceApp;Trusted_Connection=true");
+        }
+        
+        public DbSet<OtobilSaleModel> OtobilSales { get; set; }
+    }
+}
