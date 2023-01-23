b9f5bc13d95d8909886eb5d5c9979fe6a41ed4f5
Hüseyin ÇALIŞKAN <huseyinncaliskann@outlook.com>
2023-01-23 13:26:09 +0300

    Fuel card firm txn options entities added

---
+++ Entitites/Concrete/FuelCardFirmTxnOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class FuelCardFirmTxnOptions
+    {
+        public int FirmNo { get; set; }
+        public int MerchantNo { get; set; }
+        public int UserSubCode { get; set; }
+    }
+}
