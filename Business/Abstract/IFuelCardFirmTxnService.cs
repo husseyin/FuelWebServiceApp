8a46bf3b65e57ddc7957bbfa2d4358edadfdbe81
Hüseyin ÇALIŞKAN <huseyinncaliskann@outlook.com>
2023-01-23 11:57:06 +0300

    Fuel card firm txn service added

---
+++ Business/Abstract/IFuelCardFirmTxnService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results.DataResult;
+using Core.Utilities.Results.OperationResult;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IFuelCardFirmTxnService
+    {
+        IDataResult<List<FuelCardFirmTxnModel>> GetFuelCardFirmTxns(string startDate, string endDate);
+        IResult AddFuelCardFirmTxns(string startDate, string endDate);
+    }
+}
