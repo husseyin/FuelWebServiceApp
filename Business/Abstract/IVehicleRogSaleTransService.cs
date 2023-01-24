c0d09c3af270a3b7231fcca7e811459e82ac55ab
Hüseyin ÇALIŞKAN <huseyinncaliskann@outlook.com>
2023-01-24 11:56:10 +0300

    vehicle rog sale trans service and manager added

---
+++ Business/Abstract/IVehicleRogSaleTransService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results.DataResult;
+using Core.Utilities.Results.OperationResult;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IVehicleRogSaleTransService
+    {
+        IDataResult<List<VehicleRogSaleTransModel>> GetVehicleRogSaleTrans(string startDate, string endDate);
+        IResult AddVehicleRogSaleTrans(string startDate, string endDate);
+    }
+}
---
+++ Business/Concrete/VehicleRogSaleTransManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.DataResult;
+using Core.Utilities.Results.OperationResult;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class VehicleRogSaleTransManager : IVehicleRogSaleTransService
+    {
+        public IResult AddVehicleRogSaleTrans(string startDate, string endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<VehicleRogSaleTransModel>> GetVehicleRogSaleTrans(string startDate, string endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
